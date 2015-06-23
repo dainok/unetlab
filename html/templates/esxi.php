d2ee70cacef8cb88d0220038aad62872bf9ed835
Andrea Dainese <andrea.dainese@gmail.com>
2015-06-23 13:48:15 +0200

    WIP

--- html/templates/esxi.php
+++ html/templates/esxi.php
@@ -38,6 +38,5 @@ $p['ram'] = 4096;
 $p['ethernet'] = 4; 
 $p['console'] = 'vnc';
 $p['qemu_arch'] = 'x86_64';
-$p['qemu_version'] = '2.1.2-esxi';
 $p['qemu_options'] = '-machine pc,accel=kvm -serial none -nographic -nodefconfig -nodefaults -display none -vga std -rtc base=utc -no-shutdown';
 ?>
