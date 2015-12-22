0ff5e099564bfee72e2b7c4a96a8ecbf718afb19
Valentin Zlidnev <zlidnev.valentin.dunice@gmail.com>
2015-12-22 18:14:28 +0300

    FIX for startup-config

--- html/themes/default/js/actions.js
+++ html/themes/default/js/actions.js
@@ -212,7 +212,7 @@ $(document).on('click', '.action-configget', function(e) {
 	var id = $(this).attr('data-path');
 	$.when(getNodeConfigs(id)).done(function(config) {
 		printFormNodeConfigs(config);
-		('#form-node-config').find('.form-control').focusout(function(){
+		('#config-data').find('.form-control').focusout(function(){
 			saveLab();
 		})
 	}).fail(function(message) {
