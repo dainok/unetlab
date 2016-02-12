499f414c5541679b8435735a791ec605801edd00
rusman85 <rusmanking@gmail.com>
2016-02-12 20:55:01 +0200

    Update vmx.php
    Change network adapter to virtio.

--- html/templates/vmx.php
+++ html/templates/vmx.php
@@ -38,5 +38,6 @@ $p['ram'] = 2048;
 $p['ethernet'] = 6;
 $p['console'] = 'telnet';
 $p['qemu_arch'] = 'x86_64';
+$p['qemu_nic'] = 'virtio-net-pci';
 $p['qemu_options'] = '-machine type=pc-1.0,accel=kvm -serial mon:stdio -nographic';
 ?>
