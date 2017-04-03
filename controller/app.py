3330cbeed4e67cc2f14fa543bbbff3a95aaf19a3
Andrea Dainese <andrea.dainese@gmail.com>
2017-04-03 18:15:24 +0200

    20170403

---
+++ controller/app.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+""" API
+    Methods:
+    - GET /api/objects - Retrieves a list of objects
+    - GET /api/objects/1 - Retrieves a specific objects
+    - POST /api/objects - Creates a new object
+    - PUT /api/objects/1 - Edits a specific object
+    - DELETE /api/objects/1 - Deletes a specific object
+    Return codes:
+    - 200 success - Request ok
+    - 201 success - New objects has been created
+    - 400 bad request - Input request not valid
+    - 401 unauthorized - User not authenticated
+    - 403 forbidden - User authenticated but not authorized
+    - 404 fail - Url or object not found
+    - 405 fail - Method not allowed
+    - 406 fail - Not acceptable
+    - 409 fail - Object already exists, cannot create another one
+    - 422 fail - Input data missing or not valid
+    - 500 error - Server error, maybe a bug/exception or a backend/database error
+"""
+__author__ = 'Andrea Dainese <andrea.dainese@gmail.com>'
+__copyright__ = 'Andrea Dainese <andrea.dainese@gmail.com>'
+__license__ = 'https://creativecommons.org/licenses/by-nc-nd/4.0/legalcode'
+__revision__ = '20170403'
+
+import flask
+from config import *
+
+config_file = '/data/etc/controller.ini'
+
+app = flask.Flask(__name__)
+
+# Loading configuration from file
+config = loadConfig(config_file)
+
+print(config)
+
+if __name__ == '__main__':
+    app.run(host = '0.0.0.0', port = 5000, extra_files = [config_file], debug = True)
+
---
+++ controller/config.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+__author__ = 'Andrea Dainese <andrea.dainese@gmail.com>'
+__copyright__ = 'Andrea Dainese <andrea.dainese@gmail.com>'
+__license__ = 'https://creativecommons.org/licenses/by-nc-nd/4.0/legalcode'
+__revision__ = '20170403'
+
+
+def loadConfig(config_file):
+    import configparser, os, sys
+
+    if not os.path.isfile(config_file):
+        # File is not present
+        try:
+            # Write an empty file
+            open(config_file, 'a').close()
+        except Exception as err:
+            # Cannot write the configuration file
+            sys.stderr.write('Cannot create configuration file "{}"\n'.format(config_file))
+            sys.exit(1)
+
+    # Loading Config
+    config = configparser.ConfigParser()
+    need_to_save = False
+
+    # Setting default values
+    if not config.has_section('app'):
+        config.add_section('app')
+        need_to_save = True
+    if not config.has_section('controller'):
+        config.add_section('controller')
+        need_to_save = True
+    if not config.has_section('advanced'):
+        config.add_section('advanced')
+        need_to_save = True
+    if not config.has_option('app', 'database_uri'):
+        config['app']['database_uri'] = 'mysql://root:eve-ng@localhost/unetlab'
+        need_to_save = True
+    if not config.has_option('app', 'debug'):
+        config['app']['debug'] = 'False'
+        need_to_save = True
+    if not config.has_option('app', 'port'):
+        config['app']['port'] = '5000'
+        need_to_save = True
+    if not config.has_option('app', 'testing'):
+        config['app']['testing'] = 'False'
+        need_to_save = True
+    if not config.has_option('app', 'lab_extension'):
+        config['app']['lab_extension'] = 'unl'
+        need_to_save = True
+    if not config.has_option('app', 'lab_repository'):
+        config['app']['lab_repository'] = '/data/labs'
+        need_to_save = True
+    if not config.has_option('controller', 'id'):
+        config['controller']['id'] = '0'
+        need_to_save = True
+    if not config.has_option('advanced', 'label_length'):
+        config['advanced']['label_length'] = '2'
+        need_to_save = True
+    if not config.has_option('advanced', 'interface_length'):
+        config['advanced']['interface_length'] = '1'
+        need_to_save = True
+    if not config.has_option('advanced', 'controller_length'):
+        config['advanced']['controller_length'] = '1'
+        need_to_save = True
+
+    if need_to_save:
+        # Need to update configuration file
+        try:
+            with open(config_file, 'w') as config_fd:
+                config.write(config_fd)
+                config_fd.close()
+        except Exception as err:
+            # Cannot update the configuration file
+            sys.stderr.write('Cannot update configuration file "{}"\n'.format(config_file))
+            sys.exit(1)
+
+    # Parsing
+    result = config._sections
+    result['app']['debug'] = config.getboolean('app', 'debug')
+    result['app']['port'] = config.getint('app', 'port')
+    result['app']['testing'] = config.getboolean('app', 'testing')
+    result['controller']['id'] = config.getint('controller', 'id')
+    result['advanced']['label_length'] = config.getint('advanced', 'label_length')
+    result['advanced']['interface_length'] = config.getint('advanced', 'interface_length')
+    result['advanced']['controller_length'] = config.getint('advanced', 'controller_length')
+
+    return result
+
