1c06a380ae27d3084d0aded6c9273eacfd794a6b
Andrea Dainese <andrea.dainese@gmail.com>
2017-04-28 14:57:59 +0200

    20170428

--- flask_apps/controller/__init__.py
+++ flask_apps/controller/__init__.py
@@ -26,14 +26,18 @@ __revision__ = '20170403'
     - 500 error - Server error, maybe a bug/exception or a backend/database error
 """
 
-import hashlib, memcache, os, sh, shutil, sys
+import hashlib, memcache, os, sh, shutil, socket, sys
 from flask import Flask
 from flask_migrate import Migrate, MigrateCommand
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
-from flask_script import Manager
+from flask_script import Command, Manager
 from celery import Celery
 from controller.config import *
+
+class runCelery(Command):
+    def run(self):
+        celery.worker_main(['controller.worker', '--loglevel=INFO'])
 
 def make_celery(app):
     celery = Celery(app.import_name, backend = app.config['CELERY_RESULT_BACKEND'], broker = app.config['CELERY_BROKER_URL'])
@@ -76,7 +80,7 @@ cache = memcache.Client([config['app']['memcache_server']], debug = 0)
 celery = make_celery(app)
 api_key = config['app']['api_key']
 manager = Manager(app)
-#TODO manager.add_command('celery', run_celery)
+manager.add_command('celery', runCelery())
 #TODO manager.add_command('db', MigrateCommand)
 
 # Postpone to avoid circular import
@@ -129,9 +133,20 @@ if not os.path.isdir('{}/local'.format(config['app']['lab_repository'])):
 repository = RepositoryTable.query.get('local')
 if not repository:
     repository = RepositoryTable(
-        repository = 'local'
+        id = 'local'
     )
     db.session.add(repository)
+    db.session.commit()
+
+# Adding local controller if not present
+controller = ControllerTable.query.get(config['controller']['id'])
+if not controller:
+    controller = ControllerTable(
+        id = config['controller']['id'],
+        inside_ip = socket.gethostbyname(socket.gethostname()),
+        master = True
+    )
+    db.session.add(controller)
     db.session.commit()
 
 # Routing
@@ -139,5 +154,6 @@ api.add_resource(Auth, '/api/v1/auth')
 api.add_resource(Lab, '/api/v1/labs', '/api/v1/labs/<string:lab_id>')
 api.add_resource(Repository, '/api/v1/repositories', '/api/v1/repositories/<string:repository>')
 api.add_resource(Role, '/api/v1/roles', '/api/v1/roles/<string:role>')
+api.add_resource(Routing, '/api/v1/routing')
 api.add_resource(Task, '/api/v1/tasks', '/api/v1/tasks/<string:task_id>')
 api.add_resource(User, '/api/v1/users', '/api/v1/users/<string:username>')
