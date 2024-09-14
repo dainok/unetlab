d639e25e0217e3dc5489f805a53196e307c2586b
Andrea Dainese <andrea.dainese@pm.me>
2024-09-14 19:43:37 +0200

    add host model

---
+++ unetlab/unetlab/models.py
@@ -0,0 +1,52 @@
+"""Models (ORM).
+
+Define ORM models for UNetLab objects:
+* A Host represents a Proxmox host.
+* A Template represents a Proxmox template used in UNetLab.
+* A Node represents a Proxmox virtual machine used in UNetLab.
+* A Link represents a Proxmox virtual network used in UNetLab.
+* A Lab represents an instance of a running lab.
+* A LabRepository is a directory or a Git HTTPS URL used to retrieve Labs.
+"""
+
+__author__ = "Andrea Dainese"
+__contact__ = "andrea@adainese.it"
+__copyright__ = "Copyright 2024, Andrea Dainese"
+__license__ = "GPLv3"
+
+from django.db import models
+from django.urls import reverse
+
+
+#
+# Host model
+#
+
+
+class Host(models.Model):
+    """
+    Model for Host.
+
+    The details of Proxmox hosts are retrieved and cached in this table.
+    """
+
+    address = models.CharField(primary_key=True, max_length=256)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        """Database metadata."""
+
+        db_table = "hosts"
+        db_table_comment = "Proxmox hosts"
+        ordering = ["address"]
+        verbose_name = "Host"
+        verbose_name_plural = "Hosts"
+
+    def __str__(self):
+        """Return a human readable name when the object is printed."""
+        return self.address
+
+    def get_absolute_url(self):
+        """Return the absolute url."""
+        return reverse("host-detail-view", args=[str(self.id)])
