e090131213a18f76b8f128a421b930e67d443f17
Andrea Dainese <andrea.dainese@pm.me>
2024-09-15 09:25:18 +0200

    add repository model

--- unetlab/unetlab/apps.py
+++ unetlab/unetlab/apps.py
@@ -1,9 +1,14 @@
+"""App configuration."""
+
 from django.apps import AppConfig
 
+
 class UNetLabConfig(AppConfig):
+    """Config for UNetLab Django app."""
+
     name = "unetlab"
     verbose_name = "UNetLab"
 
     def ready(self):
         """Registering signals."""
-        from . import signals
+        from . import signals  # noqa: F401
--- unetlab/unetlab/models.py
+++ unetlab/unetlab/models.py
@@ -6,7 +6,7 @@ Define ORM models for UNetLab objects:
 * A Node represents a Proxmox virtual machine used in UNetLab.
 * A Link represents a Proxmox virtual network used in UNetLab.
 * A Lab represents an instance of a running lab.
-* A LabRepository is a directory or a Git HTTPS URL used to retrieve Labs.
+* A Repository is a directory or a Git HTTPS URL used to retrieve Labs.
 """
 
 __author__ = "Andrea Dainese"
@@ -50,3 +50,40 @@ class Host(models.Model):
     def get_absolute_url(self):
         """Return the absolute url."""
         return reverse("host-detail-view", args=[str(self.id)])
+
+
+#
+# Repository model
+#
+
+
+class Repository(models.Model):
+    """
+    Model for Repository.
+
+    Store lab repository URIs and credentials.
+    """
+
+    name = models.CharField(primary_key=True, max_length=256)
+    uri = models.CharField(max_length=256)
+    username = models.CharField(max_length=256, null=True, blank=True)
+    password = models.CharField(max_length=256, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        """Database metadata."""
+
+        db_table = "repositories"
+        db_table_comment = "Lab repositories"
+        ordering = ["url"]
+        verbose_name = "Repository"
+        verbose_name_plural = "Repositories"
+
+    def __str__(self):
+        """Return a human readable name when the object is printed."""
+        return self.name
+
+    def get_absolute_url(self):
+        """Return the absolute url."""
+        return reverse("repository-detail-view", args=[str(self.id)])
