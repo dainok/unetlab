d4b7362260fec3d4504cc9edc7f3eccfd1696f43
Andrea Dainese <andrea.dainese@pm.me>
2024-09-25 16:45:37 +0200

    add wsgi

---
+++ unetlab/unetlab/wsgi.py
@@ -0,0 +1,9 @@
+"""WSGI config for unetlab project."""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "unetlab.settings")
+
+application = get_wsgi_application()
