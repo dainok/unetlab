4dc9528bdf4e0fbd9bdb050bc3640f0d9a6cf9c5
Andrea Dainese <andrea.dainese@pm.me>
2024-09-29 11:35:49 +0200

    fix static files

--- unetlab/unetlab/settings.py
+++ unetlab/unetlab/settings.py
@@ -5,6 +5,7 @@ __contact__ = "andrea@adainese.it"
 __copyright__ = "Copyright 2024, Andrea Dainese"
 __license__ = "GPLv3"
 
+import os
 from datetime import datetime
 
 from pathlib import Path
@@ -121,6 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
